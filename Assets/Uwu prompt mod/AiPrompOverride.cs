65782239f63dcf85e43445d53c312812b437de3d
Jacopo <whydoIneedToPasteEmail@godohwhy.why>
2025-08-07 23:09:28 +0200

    Removed mod builds, updated ai override to api changes

--- Assets/Uwu prompt mod/AiPrompOverride.cs
+++ Assets/Uwu prompt mod/AiPrompOverride.cs
@@ -2,15 +2,17 @@ using UCDC_Mod_Api.GameInterfaces;
 using UCDC_Mod_Api.Models;
 using UCDC_Mod_Api.Models.Localization;
 using UCDC_Mod_Api.ModInterfaces;
+using System.Collections.Generic;
 using UMod;
 
 public class AiPromptOverride : ModScript, ILocalesAiOverrider
 {
     public void SetProvider(IAiPromptProvider provider)
     {
-        AiPromptTexts overrides = new AiPromptTexts()
+        AiPromptTexts overrides = new AiPromptTexts();
+        overrides.AiPrompts = new Dictionary<EAiPrompt, string>()
         {
-            Base = "You are {0}, also known as {1}. You are an extremely kawaii cute anime girl that is overwhelmed with dopamine. You constantly use 'uwu' and overuse emotes of all sorts. You are on the desktop as an AI companion."
+            { EAiPrompt.Base , "You are {0}, also known as {1}. You are an extremely kawaii cute anime girl that is overwhelmed with dopamine. You constantly use 'uwu' and overuse emotes of all sorts. You are on the desktop as an AI companion."}
         };
 
         provider.OverridePrompt(ModHost, overrides);
