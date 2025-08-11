686ce9d6686c01bb41127894f348a8a16d418217
Jacopo <whydoIneedToPasteEmail@godohwhy.why>
2025-08-11 16:21:09 +0200

    Updated to new Mod API, added template translations override mod

---
+++ Assets/BaseTranslationMod/TranslationOverride.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using BaseTranslationMod;
+using UCDC_Mod_Api.Models.Localization;
+using UCDC_Mod_Api.ModInterfaces;
+using UMod;
+using UnityEngine;
+
+public class TranslationOverride : ModScript, ITranslationsOverrider
+{
+    private TextAsset menuCsv;
+    private TextAsset shopCsv;
+    private TextAsset tutorialCsv;
+    private TextAsset virusOffenseUiCsv;
+    private TextAsset virusOffenseCsv;
+    
+    public TranslationInfo GetTranslations()
+    {
+        TranslationInfo translationInfo = new TranslationInfo()
+        {
+            Source = ModHost,
+            LoadingMessage = "Loading Test Translations...",
+            Translations = new TranslationTable()
+        };
+        
+        menuCsv = ModAssets.Load<TextAsset>("MainUI");
+        shopCsv = ModAssets.Load<TextAsset>("Shop");
+        tutorialCsv = ModAssets.Load<TextAsset>("Tutorial");
+        virusOffenseUiCsv = ModAssets.Load<TextAsset>("VirusOffenseUI");
+        virusOffenseCsv = ModAssets.Load<TextAsset>("VirusOffense");
+
+        translationInfo.Translations.MainUi = TranslationsLoader.LoadCsvToDictionary(menuCsv);
+        translationInfo.Translations.Shop = TranslationsLoader.LoadCsvToDictionary(shopCsv);
+        translationInfo.Translations.Tutorial = TranslationsLoader.LoadCsvToDictionary(tutorialCsv);
+        translationInfo.Translations.VirusOffenseUi = TranslationsLoader.LoadCsvToDictionary(virusOffenseUiCsv);
+        translationInfo.Translations.VirusOffenseDialogues = TranslationsLoader.LoadCsvToDictionary(virusOffenseCsv);
+
+        return translationInfo;
+    }
+}
---
+++ Assets/BaseTranslationMod/TranslationsLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BaseTranslationMod
+{
+    public static class TranslationsLoader
+    {
+
+        public static Dictionary<string, string> LoadCsvToDictionary(TextAsset csvAsset)
+        {
+            CSVReader reader = new CSVReader();
+            Dictionary<string, string> distilledData = new Dictionary<string, string>();
+
+            List<List<string>> table = reader.ParseCSV(csvAsset.text);
+            foreach (var line in table)
+            {
+                if (line.Count < 4) continue;
+                
+                distilledData.Add(line[0], line[3]);
+            }
+
+            return distilledData;
+        }
+    }
+}
